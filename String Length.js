14b3cb15f47309a84c6df1240bc710f944a7ce4a
Milen03 <168510343+Milen03@users.noreply.github.com>
2024-09-09 22:57:38 +0300

    Delete String Length.js

--- String Length.js
+++
@@ -1,7 +0,0 @@
-function stringLength(str1,str2,str3){
-let sumLength=str1.length+str2.length+str3.length
-let avegareLength=Math.floor(sumLength/3)
-console.log(sumLength);
-console.log(avegareLength);
-}
-stringLength('chocolate', 'ice cream', 'cake')
